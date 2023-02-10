f53431c233cbb30219beee47b3a57cebe4f867c7
CharlieSting <101151950+CharlieSting@users.noreply.github.com>
2023-02-10 17:11:03 -0600

    COMIT
    EXPRES terminado

--- index.js
+++ index.js
@@ -284,8 +284,8 @@ app.post('/api/administrador/', (req, res) => {
     });
 
     let sql = "insert into tabla_administradores" +
-    "(nombre,telefono,correo)"+
-    "values (?,?,?)";
+        "(nombre,telefono,correo)" +
+        "values (?,?,?)";
     let parametros = [req.body.nombre, req.body.telefono, req.body.correo];
 
     con.connect(function (err) {
@@ -364,7 +364,480 @@ app.delete('/api/administrador/:id', (req, res) => {
 });
 
 
+// app.get prioridad
+app.get('/api/prioridad/', (req, res) => {
 
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
 
+    let sql = "select * from tabla_prioridad";
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, function (err, result) {
+
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.post prioridad
+app.post('/api/prioridad/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "insert into tabla_prioridad" +
+        "(prioridad)" +
+        "values (?)";
+    let parametros = [req.body.prioridad];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.put prioridad
+app.put('/api/prioridad/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "update tabla_prioridad set prioridad = ? where id_prioridad = ?";
+    let parametros = [req.body.prioridad, req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.delete prioridad
+app.delete('/api/prioridad/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "delete from tabla_prioridad where id_prioridad = ?";
+    let parametros = [req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.get estado
+app.get('/api/estado/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "select * from tabla_estado_ticket";
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, function (err, result) {
+
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.post estado
+app.post('/api/estado/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "insert into tabla_estado_ticket" +
+        "(estado)" +
+        "values (?)";
+    let parametros = [req.body.estado];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.put estado
+app.put('/api/estado/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "update tabla_estado_ticket set estado = ? where id_estado = ?";
+    let parametros = [req.body.estado, req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.delete estado
+app.delete('/api/estado/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "delete from tabla_estado_ticket where id_estado = ?";
+    let parametros = [req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.get ticket
+app.get('/api/ticket/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "select * from tabla_ticket";
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, function (err, result) {
+
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.post ticket
+app.post('/api/ticket/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "insert into tabla_ticket" +
+        "(fecha, id_estado, id_cliente, id_prioridad, asunto, descripcion_ticket, solucion)" +
+        "values (?, ?, ?, ?, ?, ?, ?)";
+    let parametros = [req.body.fecha, req.body.id_estado,
+    req.body.id_cliente, req.body.id_prioridad,
+    req.body.asunto, req.body.descripcion_ticket, req.body.solucion];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.put ticket
+app.put('/api/ticket/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "update tabla_ticket set fecha = ?, id_estado = ?, id_cliente = ?, id_prioridad = ?, asunto = ?, descripcion_ticket = ?, solucion = ? where id_ticket = ?";
+    let parametros = [req.body.fecha, req.body.id_estado,
+    req.body.id_cliente, req.body.id_prioridad,
+    req.body.asunto, req.body.descripcion_ticket, req.body.solucion, req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.delete ticket
+app.delete('/api/ticket/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "delete from tabla_ticket where id_ticket = ?";
+    let parametros = [req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.get administador_ticket
+app.get('/api/administrador_ticket/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "select * from tabla_administrador_ticket";
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, function (err, result) {
+
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.post administador_ticket
+app.post('/api/administrador_ticket/', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "insert into tabla_administrador_ticket" +
+        "(id_administrador, id_ticket)" +
+        "values (?, ?)";
+    let parametros = [req.body.id_administrador, req.body.id_ticket];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.put administador_ticket
+app.put('/api/administrador_ticket/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+    
+    let sql = "update tabla_administrador_ticket set id_administrador = ?, id_ticket = ? where id = ?";
+    let parametros = [req.body.id_administrador, req.body.id_ticket, req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+// app.delete administador_ticket
+app.delete('/api/administrador_ticket/:id', (req, res) => {
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "might361#$1Ty2Q",
+        database: "sistemTiketsDesweb1"
+    });
+
+    let sql = "delete from tabla_administrador_ticket where id = ?";
+    let parametros = [req.params.id];
+
+    con.connect(function (err) {
+
+        if (err) {
+            res.send(err);
+        } else {
+            con.query(sql, parametros, function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
 
 app.listen(3000);
