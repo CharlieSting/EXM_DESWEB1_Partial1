cf293ca20b0534b095e07afeda374120b2191672
CharlieSting <101151950+CharlieSting@users.noreply.github.com>
2023-02-10 08:22:59 -0600

    preparando

---
+++ index.js
@@ -0,0 +1,18 @@
+// inicial estandar
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+app.listen(3000);
